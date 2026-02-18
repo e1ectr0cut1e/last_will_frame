d3c69f456068d14c4994ded204ae3895fbe7d6a3
Hex <me@hex128.io>
2026-02-18 07:52:39 +0200

    yaml configuration

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module last_will_frame
 
 go 1.17
 
-require github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1
+require (
+	github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1
+	gopkg.in/yaml.v3 v3.0.1
+)
--- main.go
+++ main.go
@@ -6,39 +6,56 @@ import (
 	"log"
 	"os"
 	"os/exec"
-	"regexp"
 	"strconv"
 	"strings"
 	"time"
 
+	"gopkg.in/yaml.v3"
+
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
-func getEnvDefault(key, fallback string) string {
-	if value, ok := os.LookupEnv(key); ok {
-		return value
-	}
-	return fallback
+type Camera struct {
+	Name string `yaml:"name"`
+	URL  string `yaml:"url"`
 }
 
-var telegramToken = os.Getenv("TELEGRAM_TOKEN")
-var chatId = os.Getenv("CHAT_ID")
-var cameraAddresses = os.Getenv("RTSP_ADDRESSES")
-var rtspUsername = os.Getenv("RTSP_USERNAME")
-var rtspPassword = os.Getenv("RTSP_PASSWORD")
-var rtspUrl = getEnvDefault("RTSP_URL", "/")
-var ffmpegBin = getEnvDefault("FFMPEG_BIN", "/usr/bin/ffmpeg")
-var snapshotDir = getEnvDefault("SNAPSHOT_DIR", "/dev/shm/last_will_frame_snapshots")
-var videoDuration = getEnvDefault("VIDEO_DURATION", "10")
+type TelegramMessages struct {
+	AccessDenied   string `yaml:"access_denied"`
+	UnknownCommand string `yaml:"unknown_command"`
+	VideoFailed    string `yaml:"video_failed"`
+	SnapshotFailed string `yaml:"snapshot_failed"`
+}
 
-var numericChatId, _ = strconv.ParseInt(chatId, 10, 64)
+type TelegramCommand struct {
+	Command     string `yaml:"command"`
+	Type        string `yaml:"type"`
+	Camera      string `yaml:"camera"`
+	Description string `yaml:"description"`
+}
 
+type TelegramConfig struct {
+	Token    string            `yaml:"token"`
+	ChatId   int64             `yaml:"chat_id"`
+	Messages TelegramMessages  `yaml:"messages"`
+	Commands []TelegramCommand `yaml:"commands"`
+}
+
+type Config struct {
+	SnapshotDir   string         `yaml:"snapshot_dir"`
+	FFmpegBin     string         `yaml:"ffmpeg_bin"`
+	VideoDuration int64          `yaml:"video_duration"`
+	Cameras       []Camera       `yaml:"cameras"`
+	Telegram      TelegramConfig `yaml:"telegram"`
+}
+
+var config = Config{}
 var bot *tgbotapi.BotAPI
 
-func Capture(rtspUrl string, streamName string) {
-	m3u8Path := fmt.Sprintf("%s/%s.m3u8", snapshotDir, streamName)
-	tsDirPath := fmt.Sprintf("%s/%s", snapshotDir, streamName)
-	snapshotPath := fmt.Sprintf("%s/%s.jpg", snapshotDir, streamName)
+func Capture(streamName string, rtspUrl string) {
+	m3u8Path := fmt.Sprintf("%s/%s.m3u8", config.SnapshotDir, streamName)
+	tsDirPath := fmt.Sprintf("%s/%s", config.SnapshotDir, streamName)
+	snapshotPath := fmt.Sprintf("%s/%s.jpg", config.SnapshotDir, streamName)
 	_ = os.Remove(snapshotPath)
 	_ = os.Remove(m3u8Path)
 	_ = os.RemoveAll(tsDirPath)
@@ -54,10 +71,13 @@ func Capture(rtspUrl string, streamName string) {
 		for range [3]struct{}{} {
 			_ = os.Mkdir(tsDirPath, 0755)
 			cmd := exec.Command(
-				ffmpegBin,
-				"-y", "-timeout", "1000000", "-re", "-rtsp_transport", "tcp", "-i",
-				rtspUrl, "-an", "-c", "copy", "-f", "hls", "-hls_time", videoDuration, "-hls_list_size", "2",
-				"-hls_flags", "delete_segments", "-strftime", "1",
+				config.FFmpegBin,
+				"-y", "-timeout", "1000000", "-re", "-rtsp_transport", "tcp", "-i", rtspUrl,
+				"-an", "-c", "copy", "-f", "hls",
+				"-hls_time", strconv.FormatInt(config.VideoDuration, 10),
+				"-hls_list_size", "2",
+				"-hls_flags", "delete_segments",
+				"-strftime", "1",
 				"-hls_segment_filename", fmt.Sprintf("%s/%%Y%%m%%d%%H%%M%%S.ts", tsDirPath),
 				"-hls_base_url", fmt.Sprintf("%s/", streamName), m3u8Path,
 				"-an", "-vf", "select='eq(pict_type,PICT_TYPE_I)'",
@@ -88,29 +108,29 @@ func Capture(rtspUrl string, streamName string) {
 }
 
 func SendSnap(streamName string) {
-	snapshotPath := fmt.Sprintf("%s/%s.jpg", snapshotDir, streamName)
+	snapshotPath := fmt.Sprintf("%s/%s.jpg", config.SnapshotDir, streamName)
 	if _, err := os.Stat(snapshotPath); err == nil {
 		photoFileBytes := tgbotapi.FilePath(snapshotPath)
-		msg := tgbotapi.NewPhoto(numericChatId, photoFileBytes)
+		msg := tgbotapi.NewPhoto(config.Telegram.ChatId, photoFileBytes)
 		if _, err := bot.Send(msg); err != nil {
-			log.Println(err)
+			log.Println("Failed to send snapshot:", err)
 		}
 	} else {
-		msg := tgbotapi.NewMessage(numericChatId, "🚫 Snapshot doesn't exist")
+		msg := tgbotapi.NewMessage(config.Telegram.ChatId, config.Telegram.Messages.SnapshotFailed)
 		if _, err := bot.Send(msg); err != nil {
-			log.Println(err)
+			log.Println("Failed to send message:", err)
 		}
 	}
 }
 
 func SendVideo(streamName string) {
-	m3u8Path := fmt.Sprintf("%s/%s.m3u8", snapshotDir, streamName)
-	mp4Path := fmt.Sprintf("%s/%s.mp4", snapshotDir, streamName)
+	m3u8Path := fmt.Sprintf("%s/%s.m3u8", config.SnapshotDir, streamName)
+	mp4Path := fmt.Sprintf("%s/%s.mp4", config.SnapshotDir, streamName)
 
 	live := true
 	f, err := os.OpenFile(m3u8Path, os.O_RDONLY, os.ModePerm)
 	if err != nil {
-		log.Println(fmt.Sprintf("Error opening %s: %v", m3u8Path, err))
+		log.Println(fmt.Sprintf("Error opening %s:", m3u8Path), err)
 	} else {
 		sc := bufio.NewScanner(f)
 		for sc.Scan() {
@@ -119,29 +139,33 @@ func SendVideo(streamName string) {
 			}
 		}
 		if err := sc.Err(); err != nil {
-			log.Println(fmt.Sprintf("Error reading %s: %v", m3u8Path, err))
+			log.Println(fmt.Sprintf("Error reading %s:", m3u8Path), err)
 		}
 	}
 	_ = f.Close()
 	args := []string{"-y"}
 	if live {
-		args = append(args, "-sseof", "-"+videoDuration, "-t", videoDuration)
+		args = append(
+			args,
+			"-sseof", fmt.Sprintf("-%d", config.VideoDuration),
+			"-t", strconv.FormatInt(config.VideoDuration, 10),
+		)
 	}
 	args = append(args, "-i", m3u8Path, "-ignore_unknown", "-fflags", "+igndts", "-c", "copy", mp4Path)
 
-	cmd := exec.Command(ffmpegBin, args...)
-	log.Println(cmd.String())
+	cmd := exec.Command(config.FFmpegBin, args...)
+	log.Println("Running FFmpeg:", cmd.String())
 	_ = cmd.Run()
 	if _, err := os.Stat(mp4Path); err == nil {
 		videoFileBytes := tgbotapi.FilePath(mp4Path)
-		msg := tgbotapi.NewVideo(numericChatId, videoFileBytes)
+		msg := tgbotapi.NewVideo(config.Telegram.ChatId, videoFileBytes)
 		if _, err := bot.Send(msg); err != nil {
-			log.Println(err)
+			log.Println("Failed to send video:", err)
 		}
 	} else {
-		msg := tgbotapi.NewMessage(numericChatId, "🚫 Failed to create video")
+		msg := tgbotapi.NewMessage(config.Telegram.ChatId, config.Telegram.Messages.VideoFailed)
 		if _, err := bot.Send(msg); err != nil {
-			log.Println(err)
+			log.Println("Failed to send message:", err)
 		}
 	}
 	_ = os.Remove(mp4Path)
@@ -149,42 +173,48 @@ func SendVideo(streamName string) {
 
 func main() {
 	var err error
-	err = os.MkdirAll(snapshotDir, 0o0755)
-	if err != nil {
-		log.Fatal(err)
+	if len(os.Args) < 2 {
+		_, _ = fmt.Fprintln(os.Stderr, fmt.Sprintf("Usage: %s <CONFIG.YAML>", os.Args[0]))
+		os.Exit(1)
 	}
-	addresses := strings.Split(cameraAddresses, ",")
-	for i := 0; i < len(addresses); i++ {
-		streamName := fmt.Sprintf("snap%d", i)
+	f, err := os.OpenFile(os.Args[1], os.O_RDONLY, os.ModePerm)
+	if err != nil {
+		log.Fatalln("Failed to open the configuration file:", err)
+	}
+	decoder := yaml.NewDecoder(f)
+	err = decoder.Decode(&config)
+	_ = f.Close()
+	if err != nil {
+		log.Fatalln("Failed to decode configuration:", err)
+	}
+	err = os.MkdirAll(config.SnapshotDir, 0o0755)
+	if err != nil {
+		log.Fatalln("Failed to create snapshot directory:", err)
+	}
+	for _, camera := range config.Cameras {
 		go Capture(
-			fmt.Sprintf("rtsp://%s:%s@%s%s", rtspUsername, rtspPassword, addresses[i], rtspUrl),
-			streamName,
+			camera.Name,
+			camera.URL,
 		)
 	}
-	bot, err = tgbotapi.NewBotAPI(telegramToken)
+	bot, err = tgbotapi.NewBotAPI(config.Telegram.Token)
 	if err != nil {
-		log.Panic(err)
+		log.Fatalln("Failed to initialize Telegram:", err)
 	}
 
 	log.Println("Authorized on account:", bot.Self.UserName)
 	var commands []tgbotapi.BotCommand
-	for i := range strings.Split(cameraAddresses, ",") {
+	for _, command := range config.Telegram.Commands {
 		commands = append(commands, tgbotapi.BotCommand{
-			Command:     fmt.Sprintf("/snap%d", i),
-			Description: fmt.Sprintf("📷 get a snapshot for channel %d", i),
-		})
-	}
-	for i := range strings.Split(cameraAddresses, ",") {
-		commands = append(commands, tgbotapi.BotCommand{
-			Command:     fmt.Sprintf("/vid%d", i),
-			Description: fmt.Sprintf("📹 get a %s-second video for channel %d", videoDuration, i),
+			Command:     command.Command,
+			Description: command.Description,
 		})
 	}
 
-	scope := tgbotapi.NewBotCommandScopeChat(numericChatId)
+	scope := tgbotapi.NewBotCommandScopeChat(config.Telegram.ChatId)
 	commandsConfig := tgbotapi.NewSetMyCommandsWithScope(scope, commands...)
 	if _, err = bot.Request(commandsConfig); err != nil {
-		log.Println("Unable to set Telegram commands", err)
+		log.Println("Failed to set Telegram commands:", err)
 	}
 
 	u := tgbotapi.NewUpdate(0)
@@ -197,10 +227,10 @@ func main() {
 				fmt.Sprintf("Got message from chat %d from %s", update.Message.Chat.ID, update.Message.From),
 			)
 
-			if update.Message.Chat.ID != numericChatId {
-				msg := tgbotapi.NewMessage(update.Message.Chat.ID, "🚫 Access denied")
+			if update.Message.Chat.ID != config.Telegram.ChatId {
+				msg := tgbotapi.NewMessage(update.Message.Chat.ID, config.Telegram.Messages.AccessDenied)
 				if _, err := bot.Send(msg); err != nil {
-					log.Println(err)
+					log.Println("Failed to send message:", err)
 				}
 				continue
 			}
@@ -209,16 +239,25 @@ func main() {
 				continue
 			}
 
-			if match, err := regexp.MatchString("^snap\\d$", update.Message.Command()); err == nil && match {
-				snapNum := strings.TrimPrefix(update.Message.Command(), "snap")
-				SendSnap("snap" + snapNum)
-			} else if match, err := regexp.MatchString("^vid\\d$", update.Message.Command()); err == nil && match {
-				snapNum := strings.TrimPrefix(update.Message.Command(), "vid")
-				SendVideo("snap" + snapNum)
-			} else {
-				msg := tgbotapi.NewMessage(update.Message.Chat.ID, "🚫 Unsupported command")
+			processed := false
+			for _, command := range config.Telegram.Commands {
+				if update.Message.Command() == command.Command {
+					if command.Type == "snap" {
+						SendSnap(command.Camera)
+						processed = true
+						break
+					} else if command.Type == "video" {
+						SendVideo(command.Camera)
+						processed = true
+						break
+					}
+				}
+			}
+
+			if !processed {
+				msg := tgbotapi.NewMessage(update.Message.Chat.ID, config.Telegram.Messages.UnknownCommand)
 				if _, err := bot.Send(msg); err != nil {
-					log.Println(err)
+					log.Println("Failed to send message:", err)
 				}
 			}
 		}
